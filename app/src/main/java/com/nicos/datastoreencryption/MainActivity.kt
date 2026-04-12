f71c31298c6d64f95874c9cc04881efbeb9961e9
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-04-13 00:58:01 +0300

    prepare the UI

--- app/src/main/java/com/nicos/datastoreencryption/MainActivity.kt
+++ app/src/main/java/com/nicos/datastoreencryption/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.nicos.datastoreencryption.ui.theme.DataStoreEncryptionTheme
@@ -21,7 +24,7 @@ class MainActivity : ComponentActivity() {
             DataStoreEncryptionTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        innerPadding = innerPadding,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +34,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(innerPadding: PaddingValues, modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPadding),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Hello!",
+            modifier = modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     DataStoreEncryptionTheme {
-        Greeting("Android")
+        Scaffold {
+            Greeting(it)
+        }
     }
 }
